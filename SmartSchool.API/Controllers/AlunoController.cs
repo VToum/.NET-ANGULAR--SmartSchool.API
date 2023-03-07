583b3294a745bd27f02084cb0fad223fa3fc33b5
everl <everl@DESKTOP-2GMPD23>
2023-03-06 22:52:25 -0300

    Adicionar arquivos de projeto.

---
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+
+        public List<Aluno> Alunos = new List<Aluno>()
+        {
+            new Aluno()
+            {
+                Id = 1,
+                Nome = "João",
+                Sobrenome = "Silva",
+                Telefone = "11233233"
+            },
+            new Aluno()
+            {
+                Id = 2,
+                Nome = "Caarlos",
+                Sobrenome = "Prato",
+                Telefone = "12353324"
+            },
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+        //api/aluno/byId/{id}
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("Aluno não encontrado");
+
+            return Ok(aluno);
+        }
+
+        //api/aluno/byName?nome=nome&sobrenome=sobrenome
+        [HttpGet("byName")]
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            var aluno = Alunos.FirstOrDefault(a =>
+                a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome));
+            if (aluno == null) return BadRequest("Nome do aluno não encontrado");
+           
+
+            return Ok(aluno);
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(Aluno aluno) 
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno) 
+        {
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            return Ok();
+        }
+    }
+}
