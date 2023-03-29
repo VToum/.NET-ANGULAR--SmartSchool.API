bf9d9a8ca539c7d275c688d01d04232cfc09a5eb
Everton Leão <everleao@gmail.com>
2023-03-28 21:58:53 -0300

    Models mais relacionamentos

--- SmartSchool.API/Models/Aluno.cs
+++ SmartSchool.API/Models/Aluno.cs
@@ -9,18 +9,30 @@ namespace SmartSchool.API.Models
     {
         public Aluno(){}
 
-        public Aluno(int id, string nome, string sobrenome, string telefone)
+        public Aluno(int id,
+                     int matricula, 
+                     string nome, 
+                     string sobrenome, 
+                     string telefone, 
+                     DateTime dataNascimento)
         {
             this.Id = id;
             this.Nome = nome;
+            this.Matricula = matricula;
+            this.DataNascimento = dataNascimento;
             this.Sobrenome = sobrenome;
             this.Telefone = telefone;
         }
 
         public int Id { get; set; }
+        public int Matricula { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime? DataInicio { get; set; } = DateTime.Now;
+        public DateTime? DataFim { get; set; } = null;
+        public bool Ativo { get; set; } = true;
         public IEnumerable<AlunoDisciplina> alunoDisciplinas { get; set; }
     }
 }
--- SmartSchool.API/Models/Professor.cs
+++ SmartSchool.API/Models/Professor.cs
@@ -7,15 +7,28 @@ namespace SmartSchool.API.Models
 {
     public class Professor
     {
-        public Professor() {}
+        public Professor() { }
 
-        public Professor(int id, string nome)
+        public Professor(int id,
+                         string nome,
+                         string sobrenome,
+                         string telefone,
+                         int registro)
         {
             this.Id = id;
             this.Nome = nome;
+            this.Sobrenome = sobrenome;
+            this.Telefone = telefone;
+            this.Registro = registro;
         }
         public int Id { get; set; }
         public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Telefone { get; set; }
+        public int Registro { get; set; }
+        public DateTime DataIni { get; set; } = DateTime.Now;
+        public DateTime? DataFim { get; set; } = null;
+        public bool Ativo { get; set; } = true;
         public IEnumerable<Disciplina> Disciplinas { get; set; }
     }
 }
